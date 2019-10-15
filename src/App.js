c0b6e37fce1bb8400be14102dd40f25773971ad5
sharsheevnursultan <nursultan.sharsheev@gmail.com>
2019-10-15 11:55:57 +0600

    'version2'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./form/form";
+import Weather from "./weather/weather";
+import './App.css'
+// import Map from "./maps/maps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=abacceaec861b12e6d69c3f60c510584";
+
+class App extends React.Component {
+
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        weatherIcon: undefined,
+        sunset: undefined,
+        sunrise: undefined,
+        error: undefined,
+        notFoundCode: undefined,
+        notFoundMessage: undefined,
+
+    };
+
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        try {
+            const api_call = await fetch(`${URL}+${city}+${API_KEY}&units=metric`);
+            const weatherData = await api_call.json();
+
+            console.log(weatherData);
+            console.log(weatherData.name);
+            console.log(weatherData.cod);
+            if (city) {
+                let sunset = weatherData.sys.sunset * 1000;
+                let date = new Date();
+                date.setTime(sunset);
+                let sunsetDate = date.getHours() + ':' + date.getMinutes();
+
+                let sunrise = weatherData.sys.sunrise * 1000;
+                let date2 = new Date();
+                date2.setTime(sunrise);
+                let sunriseDate = date2.getHours() + ':' + date2.getMinutes();
+
+                this.setState({
+                    temperature: weatherData.main.temp,
+                    city: weatherData.name,
+                    country: weatherData.sys.country,
+                    humidity: weatherData.main.humidity,
+                    speed: weatherData.wind.speed,
+                    description: weatherData.weather[0].description,
+                    weatherIcon: weatherData.weather[0].icon,
+                    sunset: sunsetDate,
+                    sunrise: sunriseDate,
+                    error: undefined,
+                    notFoundCode: undefined,
+                    notFoundMessage: undefined
+
+                });
+            } else {
+                this.setState({
+                    temperature: undefined,
+                    city: undefined,
+                    country: undefined,
+                    humidity: undefined,
+                    speed: undefined,
+                    description: undefined,
+                    weatherIcon: undefined,
+                    sunset: undefined,
+                    sunrise: undefined,
+                    error: "Введите название города",
+                    notFoundCode: undefined,
+                    notFoundMessage: undefined
+                });
+            }
+
+        } catch {
+            const api_call = await fetch(`${URL}+${city}+${API_KEY}&units=metric`);
+            const weatherData = await api_call.json();
+            console.log(weatherData);
+            console.log(weatherData.name);
+            if (city) {
+                this.setState({
+                    temperature: undefined,
+                    city: undefined,
+                    country: undefined,
+                    humidity: undefined,
+                    speed: undefined,
+                    description: undefined,
+                    weatherIcon: undefined,
+                    sunset: undefined,
+                    sunrise: undefined,
+                    error: 'Имя введено неправильно',
+                    notFoundCode: weatherData.cod,
+                    notFoundMessage: weatherData.message
+                });
+            } else {
+                this.setState({
+                    notFoundCode: undefined,
+                    error: "Введите название города",
+                    notFoundMessage: undefined
+                });
+            }
+        }
+
+    };
+
+
+    render() {
+        const image_img=(<img className='left-col-bg cols-height' src={"https://source.unsplash.com/600x450/?"+this.state.city} alt="No image"/>);
+
+        return (
+            <div className='bg-color'>
+                <div className='card-holder'>
+                    <h1 className='card main-top-app-name'>Weather App</h1>
+                    <div className='row'>
+                        <div className='col-md-6 '>
+                            <div className='left-col-bg cols-height'>
+
+                                <Form getWeather={this.getWeather}/>
+                                <Weather
+                                    temperature={this.state.temperature}
+                                    humidity={this.state.humidity}
+                                    city={this.state.city}
+                                    country={this.state.country}
+                                    description={this.state.description}
+                                    weatherIcon={this.state.weatherIcon}
+                                    sunset={this.state.sunset}
+                                    sunrise={this.state.sunrise}
+                                    speed={this.state.speed}
+                                    error={this.state.error}
+                                    notFoundCode={this.state.notFoundCode}
+                                    notFoundMessage={this.state.notFoundMessage}
+                                />
+                            </div>
+                        </div>
+                        <div className='col-md-6'>
+
+                            {image_img}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        );
+
+
+    }
 }
 
 export default App;
---
+++ src/weather/weather.js
@@ -0,0 +1,84 @@
+import React from "react";
+import '../weather/weather.css'
+
+const Weather = (props) => (
+    <div>
+        <div>
+            {props.city && props.country &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    Город:
+                </span>
+                <span>{props.city}, {props.country}</span>
+            </div>}
+
+            {props.temperature &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    Температура:
+                </span>
+                <span>{props.temperature}°C</span>
+            </div>}
+            {props.humidity &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    Влажность:
+                </span>
+                <span>{props.humidity} %</span>
+            </div>}
+            {props.speed &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    Ветер:
+                </span>
+                <span>{props.speed} м/с</span>
+            </div>}
+            {props.sunrise &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                     Восход:
+                </span>
+                <span>{props.sunrise}</span></div>}
+            {props.sunset &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    Закат:
+                </span>
+                <span>{props.sunset}</span></div>}
+            {props.notFoundCode &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                </span>
+                <span>{props.notFoundCode}</span>
+            </div>}
+            {props.notFoundMessage &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+
+                </span>
+                <span>{props.notFoundMessage}</span>
+            </div>}
+            {props.error &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+                    <div>{props.error}</div>
+                </span>
+            </div>}
+        </div>
+        <div>
+            {props.weatherIcon &&
+            <div className='d-flex justify-content-between weather-padding'>
+                <span>
+
+                </span>
+                <span>
+                    <img
+                        src={'http://openweathermap.org/img/w/' + props.weatherIcon + '.png'}
+                        alt={props.description}/>
+                </span>
+            </div>}
+        </div>
+    </div>
+);
+
+export default Weather;
