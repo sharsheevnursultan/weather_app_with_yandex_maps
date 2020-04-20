d2ac93234da7e3a33cb5541dea4c93f7e291daa0
Nursultan <nursultan.sharsheev@gmail.com>
2020-04-20 19:07:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Form from "./form/form";
 import Weather from "./weather/weather";
 import './App.css'
 import {YMaps, Map} from 'react-yandex-maps';
-
+import Header from "./header/Header";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const API_KEY = "&appid=abacceaec861b12e6d69c3f60c510584";
+const LANGUAGE = "&lang=ru"
+const UNITS = "&units=metric"
 
 class App extends React.Component {
-
     state = {
         temperature: undefined,
         city: undefined,
@@ -25,15 +26,12 @@ class App extends React.Component {
         coord_lat: undefined,
         coord_lon: undefined
     };
-
-
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         try {
-            const api_call = await fetch(`${URL}+${city}+${API_KEY}&units=metric`);
+            const api_call = await fetch(`${URL}${city}${API_KEY}${LANGUAGE}${UNITS}`);
             const weatherData = await api_call.json();
-
             console.log(weatherData);
             console.log(weatherData.name);
             console.log(weatherData.cod);
@@ -42,12 +40,10 @@ class App extends React.Component {
                 let date = new Date();
                 date.setTime(sunset);
                 let sunsetDate = date.getHours() + ':' + date.getMinutes();
-
                 let sunrise = weatherData.sys.sunrise * 1000;
                 let date2 = new Date();
                 date2.setTime(sunrise);
                 let sunriseDate = date2.getHours() + ':' + date2.getMinutes();
-
                 this.setState({
                     temperature: weatherData.main.temp,
                     city: weatherData.name,
@@ -75,7 +71,7 @@ class App extends React.Component {
                     weatherIcon: undefined,
                     sunset: undefined,
                     sunrise: undefined,
-                    error: "Enter city name",
+                    error: "Введите имя города",
                     notFoundCode: undefined,
                     notFoundMessage: undefined,
                     coord_lat: undefined,
@@ -83,9 +79,8 @@ class App extends React.Component {
 
                 });
             }
-
         } catch {
-            const api_call = await fetch(`${URL}+${city}+${API_KEY}&units=metric`);
+            const api_call = await fetch(`${URL}${city}${API_KEY}${LANGUAGE}${UNITS}`);
             const weatherData = await api_call.json();
             console.log(weatherData);
             console.log(weatherData.name);
@@ -100,118 +95,50 @@ class App extends React.Component {
                     weatherIcon: undefined,
                     sunset: undefined,
                     sunrise: undefined,
-                    error: 'Invalid city name',
+                    error: 'Неверное имя города',
                     notFoundCode: weatherData.cod,
                     notFoundMessage: weatherData.message
                 });
             } else {
                 this.setState({
                     notFoundCode: undefined,
-                    error: "Enter city name",
+                    error: "Введите имя города",
                     notFoundMessage: undefined
                 });
             }
         }
     };
 
-
     render() {
-        let myTumbler = (elem, elem2, elem3, elem4, elem5) => {
-
-                elem = document.getElementById('color_toggle');
-                if (elem.classList.contains('bg-color')) {
-                    elem.classList.replace('bg-color', 'black-bg-color-without-border')
-                } else if (elem.classList.contains('black-bg-color-without-border')) {
-                    elem.classList.replace('black-bg-color-without-border', 'bg-color')
-                }
-
-                elem2 = document.getElementById('color_toggle2');
-                if (elem2.classList.contains('main-top-app-name')) {
-                    elem2.classList.replace('main-top-app-name', 'app-name-black-bg-color');
-                } else if (elem2.classList.contains('app-name-black-bg-color')) {
-                    elem2.classList.replace('app-name-black-bg-color', 'main-top-app-name');
-                }
-
-                elem3 = document.getElementById('color_toggle3');
-                if (elem3.classList.contains('left-col-bg')) {
-                    elem3.classList.replace('left-col-bg', 'black-bg-color');
-                } else if (elem3.classList.contains('black-bg-color')) {
-                    elem3.classList.replace('black-bg-color', 'left-col-bg');
-                }
-
-                elem4 = document.getElementById('color_toggle4');
-                if (elem4.classList.contains('yin-yang-tumbler')) {
-                    elem4.classList.replace('yin-yang-tumbler', 'white-yin-yang-tumbler');
-                } else if (elem4.classList.contains('white-yin-yang-tumbler')) {
-                    elem4.classList.replace('white-yin-yang-tumbler', 'yin-yang-tumbler');
-                }
-
-                elem5 = document.getElementById('color_toggle5');
-                if (elem5.classList.contains('left-col-bg')) {
-                    elem5.classList.replace('left-col-bg', 'black-bg-color');
-                } else if (elem5.classList.contains('black-bg-color')) {
-                    elem5.classList.replace('black-bg-color', 'left-col-bg');
-                }
-            }
-        ;
-
         return (
             <div id='color_toggle' className='bg-color'>
                 <div className='card-holder'>
-                    <h1 id='color_toggle2' className='card main-top-app-name'>Weather App
-                        <span onClick={myTumbler} title='Dark mode'>
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="yin-yang"
-                                 className="yin-yang-tumbler" role="img" id='color_toggle4'
-                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-                                <path
-                                    d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 376c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm0-128c-53.02 0-96 42.98-96 96s42.98 96 96 96c-106.04 0-192-85.96-192-192S141.96 64 248 64c53.02 0 96 42.98 96 96s-42.98 96-96 96zm0-128c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32z"></path>
-                            </svg></span>
-                    </h1>
+                    <Header/>
                     <div className='row'>
                         <div className='col-md-6 '>
                             <div id='color_toggle3' className='left-col-bg cols-height'>
-
                                 <Form getWeather={this.getWeather}/>
-                                <Weather
-                                    temperature={this.state.temperature}
-                                    humidity={this.state.humidity}
-                                    city={this.state.city}
-                                    country={this.state.country}
-                                    description={this.state.description}
-                                    weatherIcon={this.state.weatherIcon}
-                                    sunset={this.state.sunset}
-                                    sunrise={this.state.sunrise}
-                                    speed={this.state.speed}
-                                    error={this.state.error}
-                                    notFoundCode={this.state.notFoundCode}
-                                    notFoundMessage={this.state.notFoundMessage}
-                                />
+                                <Weather state={this.state}/>
                             </div>
                         </div>
                         <div className='col-md-6'>
                             <div id='color_toggle5' className='left-col-bg cols-height'>
                                 <YMaps>
-                                    <div>
-                                        <Map className='ymaps-size'
-                                             state={{
-                                                 center: [this.state.coord_lat, this.state.coord_lon],
-                                                 zoom: 9,
-                                                 controls: ['zoomControl', 'fullscreenControl'],
-                                             }}
-                                             modules={['control.ZoomControl', 'control.FullscreenControl']}
-                                        />
-                                    </div>
+                                    <Map className='ymaps-size'
+                                         state={{
+                                             center: [this.state.coord_lat, this.state.coord_lon],
+                                             zoom: 9,
+                                             controls: ['zoomControl', 'fullscreenControl'],
+                                         }}
+                                         modules={['control.ZoomControl', 'control.FullscreenControl']}
+                                    />
                                 </YMaps>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         );
-
-
     }
 }
 
--- src/weather/weather.js
+++ src/weather/weather.js
@@ -1,84 +1,81 @@
 import React from "react";
 import '../weather/weather.css'
 
-const Weather = (props) => (
-    <div>
+const Weather = (props) => {
+    let state = {
+        temperature: props.state.temperature,
+        humidity: props.state.humidity,
+        city: props.state.city,
+        country: props.state.country,
+        description: props.state.description,
+        weatherIcon: props.state.weatherIcon,
+        sunset: props.state.sunset,
+        sunrise: props.state.sunrise,
+        speed: props.state.speed,
+        error: props.state.error,
+        notFoundCode: props.state.notFoundCode,
+        notFoundMessage: props.state.notFoundMessage,
+    }
+    return (
         <div>
-            {props.city && props.country &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    City:
-                </span>
-                <span>{props.city}, {props.country}</span>
-            </div>}
-
-            {props.temperature &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    Temp:
-                </span>
-                <span>{props.temperature} °C</span>
-            </div>}
-            {props.humidity &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    Humidity:
-                </span>
-                <span>{props.humidity} %</span>
-            </div>}
-            {props.speed &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    Wind speed:
-                </span>
-                <span>{props.speed} m/s</span>
-            </div>}
-            {props.sunrise &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                     Sunrise:
-                </span>
-                <span>{props.sunrise}</span></div>}
-            {props.sunset &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    Sunset:
-                </span>
-                <span>{props.sunset}</span></div>}
-            {props.notFoundCode &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                </span>
-                <span>{props.notFoundCode}</span>
-            </div>}
-            {props.notFoundMessage &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-
-                </span>
-                <span>{props.notFoundMessage}</span>
-            </div>}
-            {props.error &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-                    <div>{props.error}</div>
-                </span>
-            </div>}
-        </div>
-        <div>
-            {props.weatherIcon &&
-            <div className='d-flex justify-content-between weather-padding'>
-                <span>
-
-                </span>
-                <span>
+            <div>
+                {state.city && state.country &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Город:</span>
+                    <span>{state.city}, {state.country}</span>
+                </div>}
+                {state.temperature &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Температура:</span>
+                    <span>{state.temperature} °C</span>
+                </div>}
+                {state.humidity &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Влажность:</span>
+                    <span>{state.humidity} %</span>
+                </div>}
+                {state.speed &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Скорость ветра:</span>
+                    <span>{state.speed} м/с</span>
+                </div>}
+                {state.sunrise &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Восход:</span>
+                    <span>{state.sunrise}</span></div>}
+                {state.sunset &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span>Закат:</span>
+                    <span>{state.sunset}</span>
+                </div>}
+                {state.notFoundCode &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span> </span>
+                    <span>{state.notFoundCode}</span>
+                </div>}
+                {state.notFoundMessage &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span> </span>
+                    <span>{state.notFoundMessage}</span>
+                </div>}
+                {state.error &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span><div>{state.error}</div></span>
+                </div>}
+            </div>
+            <div>
+                {state.weatherIcon &&
+                <div className='d-flex justify-content-between weather-padding'>
+                    <span> </span>
+                    <span>
                     <img
-                        src={'https://openweathermap.org/img/w/' + props.weatherIcon + '.png'}
-                        alt={props.description}/>
+                        src={'https://openweathermap.org/img/w/' + state.weatherIcon + '.png'}
+                        alt={state.description}/>
                 </span>
-            </div>}
+                </div>}
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Weather;
